87ef38bef72370498f4594e82cef93e1b48d9cba
Lucas <lucas.balduino@sempreceub.com>
2024-09-19 19:01:13 -0300

    Exercicio 6

--- ListaDeExecicios1/Exercicio6.py
+++ ListaDeExecicios1/Exercicio6.py
@@ -3,91 +3,70 @@ biblioteca. O programa deve permitir adicionar um novo
 livro (como uma lista contendo título, autor e ano), 
 listar todos os livros, e permitir a busca de livros
 pelo título'''
+# Array para armazenar os livros
+biblioteca = []
 
-import random
+# Função para adicionar um livro
+def adicionar_livro(titulo, autor, ano):
+    livro = [titulo, autor, ano] #Cria um array com as informacoes entregues pelo usuario
+    biblioteca.append(livro) #Adiciona ao array de Livros
+    print(f'Livro "{titulo}" adicionado com sucesso!') #Mensagem falando que adicionou o livro
 
-escolha = 0
-contadorEmpate = 0
-contadorVitoria = 0
-contadorDerrota = 0
-
-while escolha != -1:
-    
-#Gerador da escolha do Computador
-    probabilidade = random.randint(1,100)
-    print(probabilidade)
-    if probabilidade < 34:
-        escolhaComputador = 1
-    elif probabilidade < 67:
-        escolhaComputador = 2
+# Função para listar todos os livros
+def listar_livros():
+    if biblioteca != []: # Se a biblioteca nao estiver vazia
+        print("\nLista de livros:")
+        for i in range(len(biblioteca)):
+            print(f"{i}. Título: {biblioteca[i][0]}, Autor: {biblioteca[i][1]}, Ano: {biblioteca[i][2]}")
     else:
-        escolhaComputador = 3
+        print("\nNenhum livro na biblioteca.")
 
-#Interface de Escolha
-    print("/////////////////////////////////")
-    print("//Jogo de Pedra Papel e Tesoura//")
-    print("/////////////////////////////////")
-    print("//          Insira:            //")
-    print("//        1 - Pedra            //")
-    print("//        2 - Papel            //")
-    print("//        3 - Tesoura          //")
-    print("//       -1 - Sair             //")
-    print("/////////////////////////////////")
+# Função para buscar um livro pelo título
+def buscar_livro(titulo):
+    encontrados = []
+    for i in range(len(biblioteca)):
+        if titulo == biblioteca[i][0]:
+            encontrados.append(biblioteca[i])
+    if encontrados != []:
+        print("\nLivros encontrados:")
+        for i in range(len(encontrados)):
+             print(f"{i}. Título: {encontrados[i][0]}, Autor: {encontrados[i][1]}, Ano: {encontrados[i][2]}")
+    else:
+        print(f'\nNenhum livro com o título "{titulo}" encontrado.')
 
-#Entrada de Dados
-    escolha = int(input())
+# Menu principal
+def menu():
+    while True:
+        print("/////////////////////////////////")
+        print("/////////////Menu////////////////")
+        print("/////////////////////////////////")
+        print("//          Insira:            //")
+        print("//       1. Adicionar livro    //")
+        print("//  2. Listar todos os livros  //")
+        print("// 3. Buscar livro por título  //")
+        print("//       -1 - Sair             //")
+        print("/////////////////////////////////")
+        
+        opcao = input("Escolha uma opção: ")
 
-#Casos de Empate
-    
-    if (escolhaComputador == 1 and escolha == 1):
-        print("O computador tambem escolheu Pedra!")
-        print("Voces Empataram!")
-        contadorEmpate +=1
-    if (escolhaComputador == 2 and escolha == 2):
-        print("O computador tambem escolheu Papel!")
-        print("Voces Empataram!")
-        contadorEmpate +=1
-    if (escolhaComputador == 3 and escolha == 3):
-        print("O computador tambem escolheu Tesoura!")
-        print("Voces Empataram!")
-        contadorEmpate +=1
+        if opcao == '1':
+            titulo = input("Digite o título do livro: ")
+            autor = input("Digite o autor do livro: ")
+            ano = input("Digite o ano de publicação: ")
+            adicionar_livro(titulo, autor, ano)
 
-#Casos de Derrota
-    if (escolhaComputador == 1 and escolha == 3):
-        print("O computador escolheu Pedra!")
-        print("Pedra ganha de Tesoura!")
-        print("Voce Perdeu!")
-        contadorDerrota +=1
-    if (escolhaComputador == 2 and escolha == 1):
-        print("O computador escolheu Papel!")
-        print("Papel ganha de Pedra!")
-        print("Voce Perdeu!")
-        contadorDerrota +=1
-    if (escolhaComputador == 3 and escolha == 2):
-        print("O computador escolheu Tesoura!")
-        print("Tesoura ganha de Papel!")
-        print("Voce Perdeu!")
-        contadorDerrota +=1
+        elif opcao == '2':
+            listar_livros()
 
-#Casos de Vitoria
-    if (escolha == 1 and escolhaComputador == 3):
-        print("O computador escolheu Tesoura!")
-        print("Pedra ganha de Tesoura!")
-        print("Voce Ganhou!")
-        contadorVitoria +=1
-    
-    if (escolha == 2 and escolhaComputador == 1):
-        print("O computador escolheu Pedra!")
-        print("Papel ganha de Pedra!")
-        print("Voce Ganhou!")
-        contadorVitoria +=1
-    
-    if (escolha == 3 and escolhaComputador == 2):
-        print("O computador escolheu Papel!")
-        print("Tesoura ganha de Papel!")
-        print("Voce Ganhou!")
-        contadorVitoria +=1
+        elif opcao == '3':
+            titulo = input("Digite o título do livro para buscar: ")
+            buscar_livro(titulo)
 
-print(f"Voce ganhou {contadorVitoria} vezes!")
-print(f"Voce perdeu {contadorDerrota} vezes!")
-print(f"Voce empatou {contadorEmpate} vezes!")
+        elif opcao == '4':
+            print("Saindo do programa...")
+            break
+        else:
+            print("Opção inválida. Tente novamente.")
+
+# Executar o menu
+menu()
